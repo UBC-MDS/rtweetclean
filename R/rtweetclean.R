aa0b16c754526b97a6d6ebf3eed702fcbd2baa49
Cal Schafer <schafer1@student.ubc.ca>
2021-03-06 14:15:08 -0700

    added docstring for sentiment_total()

--- R/rtweetclean.R
+++ R/rtweetclean.R
@@ -16,6 +16,32 @@ tweet_words <- function(clean_dataframe, top_n=1) {
 
 }
 
+sentiment_total <- function(tweets, drop_sentiment == FALSE) {
+
+#' Sentiment Word Counts
+#'
+#'Takes an input of of single english words and outputs the number of words associated
+#'with eight emotions and positive/negative sentiment. This is based on the the
+#'crowd-sourced NRC Emotion Lexicon, which associates words with eight basic emotions
+#'(anger, fear, anticipation, trust, surprise, sadness, joy, and disgust) and two
+#'sentiments (negative and positive). For more information on NRC:
+#'http://saifmohammad.com/WebPages/NRC-Emotion-Lexicon.htm
+
+#'Note that words can be 0:n with emotions (either associated with none, 1, or many).
+#'
+#' @param tweets 1-column dataframe
+#' @param drop_sentiment. A true/false bool that drops sentiment rows if no words are
+#'  associated with that sentiment
+#'
+#' @return dataframe
+#' @export
+#'
+#' @examples
+#' sentiment_total(df['tweets'], drop_sentiment = FALSE)
+}
+
+
+engagement_by_hour <- function(tweets_df) {
 
 #' Average engagement by hour
 #'
