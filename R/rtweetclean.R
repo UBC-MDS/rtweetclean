20b32313d9858c8e2e38286b40767567dfc9bfc5
Matthew Pin <MattTPin@gmail.com>
2021-03-06 10:46:58 -0800

    added clean_df docstring

--- R/rtweetclean.R
+++ R/rtweetclean.R
@@ -1,3 +1,43 @@
+#' Adds new column(s) to dataframe returned by rtweet get_timeline() function with
+#' default parameters based on user specified input
+#'
+#'Returns a new dataframe containing additional columns that were not in the original
+#'Generatable columns include...
+#'  handle: contains specified twitter handle, (default is none and adds no column)
+#'  text_only: strips emojis, hashtags, and hyperlinks from the text column
+#'  word_count: counts the number of words contained in the text_only column
+#'  emojis: contains the extracted emojis from text
+#'  hashtags: contains the extracted hashtags from text
+#'  sentiment: contains the a sentiment polarity score
+#'  flesch_readability: contains the a flesch readability score
+#'  proportion_of_avg_retweets: a proportion value of how many retweets a tweet received compared to the account average 
+#'  proportion_of_avg_favorites: a proportion value of how many favorites a tweet received compared to the account average 
+#'
+#'alphabetical order for any word ties.
+#'
+#' @param raw_tweets_df dataframe
+#' @param handle str
+#' @param text_only bool
+#' @param word_count bool
+#' @param emojis bool
+#' @param hashtags bool
+#' @param sentiment bool
+#' @param flesch_readability bool
+#' @param proportion_of_avg_retweets bool
+#' @param proportion_of_avg_favorites bool
+#'
+#' @return dataframe
+#' @export
+#'
+#' @examples
+#' function(raw_tweets_df)
+#' function(raw_tweets_df, handle = "Canucks")
+#' function(raw_tweets_df, handle = "Canucks", word_count = FALSE)
+clean_df <- function(raw_tweets_df, handle = "", text_only = TRUE, word_count = TRUE, emojis = TRUE, hashtags = TRUE, sentiment = TRUE, flesch_readability = TRUE, proportion_of_avg_retweets = TRUE, proportion_of_avg_favorites = TRUE) {
+
+}
+
+
 #' Most common words
 #'
 #'Returns the most common words and counts from a list of tweets.
