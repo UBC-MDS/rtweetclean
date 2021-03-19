cb94117334c6af0fbebf3c407f64c91b7aced922
Matthew Pin <MattTPin@gmail.com>
2021-03-19 10:39:21 -0700

    removed pdfs

--- data-raw/nrc_emotion_lexicon.R
+++ data-raw/nrc_emotion_lexicon.R
@@ -5,6 +5,9 @@ file.remove("NRC-Emotion-Lexicon/NRC - Sentiment Lexicon - Research EULA Sept 20
 file.remove("NRC-Emotion-Lexicon/NRC-Emotion-Lexicon-v0.92/NRC-Emotion-Lexicon-v0.92-In105Languages-Nov2017Translations.xlsx")
 file.remove("NRC-Emotion-Lexicon/NRC-Emotion-Lexicon-v0.92/Older Versions/NRC-Emotion-Lexicon-v0.92-InManyLanguages.xlsx")
 file.remove("NRC-Emotion-Lexicon/NRC-Emotion-Lexicon-v0.92/Older Versions/readme.txt")
+file.remove("NRC-Emotion-Lexicon/EmoLex-Ethics-Data-Statement.pdf")
+file.remove("NRC-Emotion-Lexicon/NRC-Emotion-Lexicon-v0.92/Paper1_NRC_Emotion_Lexicon.pdf")
+file.remove("NRC-Emotion-Lexicon/NRC-Emotion-Lexicon-v0.92/Paper2_NRC_Emotion_Lexicon.pdf")
 file.remove("NRC-Emotion-Lexicon/NRC-Emotion-Lexicon-v0.92/Older Versions")
 nrc_emotion_lexicon <- readr::read_table2("NRC-Emotion-Lexicon/NRC-Emotion-Lexicon-v0.92/NRC-Emotion-Lexicon-Wordlevel-v0.92.txt",
                                           col_names = c("text_only", "sentiment", "count"))
